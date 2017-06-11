d9e5f5c9f7a30e25fdc0c06f357f1f23f5adce95
Chris <kitecabrinha@gmail.com>
2017-06-11 14:19:10 -0400

    Finished nickification

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function runEqual() {
   var total = eval(holder);
   setValue(total);
 }
-function runAdd() {
+function runMult() {
   holder = holder.concat("*");
   setValue(holder);
 }
@@ -20,9 +20,21 @@ function runSqrrt() {
   var total = eval(holder);
   var sqrRoot = Math.sqrt(total);
   setValue(sqrRoot);
-
 }
+
 function setValue(setValue) {
   var newValue =  document.getElementById('numberBar');
   newValue.value = setValue;
+}
+function runMove() {
+   nicksCageImg.style.position = "relative";
+   nicksCageImg.style.left += "70%";
+}
+function runCageTime(){
+  document.body.style.backgroundImage = "url('https://www.placecage.com/200/300')";
+}
+function runCageTimeCoolDown() {
+  document.body.style.backgroundImage = "";
+  nicksCageImg.style.position = "absolute";
+  nicksCageImg.style.left -= "70%";
 }
