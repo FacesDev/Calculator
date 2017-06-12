e614a53777cd64b65ecf5ef9937d36e28113d8cc
Chris <kitecabrinha@gmail.com>
2017-06-11 20:30:26 -0400

    final final commit, added images

--- main.js
+++ main.js
@@ -36,29 +36,29 @@ function runMove() {
 }
 function runCageTime(){
   document.body.style.backgroundImage = "url('http://data.whicdn.com/images/70078309/large.gif')";
-  oneButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_007.jpg)";
-  twoButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_008.jpg)";
-  threeButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_009.jpg)";
-  fourButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_006.jpg)";
-  fiveButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_005.jpg)";
-  sixButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_004.jpg)";
-  sevenButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_001.jpg)";
-  eightButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_002.jpg)";
-  nineButton.style.backgroundImage = "url(https://srv3.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-KGRlZSwZr1/image_part_003.jpg)";
+  oneButton.style.backgroundImage = "url(./images/cage7.jpg)";
+  twoButton.style.backgroundImage = "url(./images/cage8.jpg)";
+  threeButton.style.backgroundImage = "url(./images/cage9.jpg)";
+  fourButton.style.backgroundImage = "url(./images/cage6.jpg)";
+  fiveButton.style.backgroundImage = "url(./images/cage5.jpg)";
+  sixButton.style.backgroundImage = "url(./images/cage4.jpg)";
+  sevenButton.style.backgroundImage = "url(./images/cage1.jpg)";
+  eightButton.style.backgroundImage = "url(./images/cage2.jpg)";
+  nineButton.style.backgroundImage = 'url(./images/cage3.jpg)';
 
 }
-function runCageTimeCoolDown() {
-  document.body.style.backgroundImage = "";
-  nicksCageImg.style.position = "absolute";
-  nicksCageImg.style.left -= "0%";
-  oneButton.style.backgroundImage = "";
-  twoButton.style.backgroundImage = "";
-  threeButton.style.backgroundImage = "";
-  fourButton.style.backgroundImage = "";
-  fiveButton.style.backgroundImage = "";
-  sixButton.style.backgroundImage = "";
-  sevenButton.style.backgroundImage = "";
-  eightButton.style.backgroundImage = "";
-  nineButton.style.backgroundImage = "";
-
-}
+// function runCageTimeCoolDown() {
+//   document.body.style.backgroundImage = "";
+//   nicksCageImg.style.position = "absolute";
+//   nicksCageImg.style.left -= "0%";
+//   oneButton.style.backgroundImage = "";
+//   twoButton.style.backgroundImage = "";
+//   threeButton.style.backgroundImage = "";
+//   fourButton.style.backgroundImage = "";
+//   fiveButton.style.backgroundImage = "";
+//   sixButton.style.backgroundImage = "";
+//   sevenButton.style.backgroundImage = "";
+//   eightButton.style.backgroundImage = "";
+//   nineButton.style.backgroundImage = "";
+//
+// }
